3716826d5cb0b6bde38b9bd85fb1702cd5e93ff2
BLDop <141639037+BLDop@users.noreply.github.com>
2023-11-13 10:27:30 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,79 +2,33 @@
 let questions = [
   {
     numb: 1,
-    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
-    answer: "Marie Curie",
+    question: "Qual é a capital do Canadá?",
+    answer: "Ottawa",
     options: [
-      "Albert Einstein",
-      "Marie Curie",
-      "Stephen Hawking",
-      "Linus Pauling"
+      "Toronto",
+      "Vancouver",
+      "Ottawa",
+      "Montreal"
     ]
   },
   {
     numb: 2,
-    question: "Qual o país mais novo do mundo?",
-    answer: "Sudão do Sul",
+    question: "Quem escreveu 'Cem Anos de Solidão'?",
+    answer: "Gabriel García Márquez",
     options: [
-      "Timor Leste",
-      "Montenegro",
-      "Sudão do Sul",
-      "Kosovo"
+      "Isabel Allende",
+      "Mario Vargas Llosa",
+      "Gabriel García Márquez",
+      "Julio Cortázar"
     ]
   },
   {
     numb: 3,
-    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
-    answer: "Solstício de verão",
+    question: "Qual é o elemento químico mais abundante na crosta terrestre?",
+    answer: "Oxigênio",
     options: [
-      "Equinócio de outono",
-      "Solstício de inverno",
-      "Solstício de verão",
-      "Solstício de outono"
+      "Ferro",
+      "Silício",
+      "Oxigênio",
+      "Alumínio"
     ]
-  },
-  {
-    numb: 4,
-    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
-    answer: "12",
-    options: [
-      "12",
-      "11",
-      "14",
-      "13"
-    ]
-  },
-  {
-    numb: 5,
-    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
-    answer: "Vênus e Urano",
-    options: [
-      "Saturno e Júpiter",
-      "Vênus e Urano",
-      "Terra e Marte",
-      "Mercúrio e Saturno"
-    ]
-  },
-  {
-    numb: 6,
-    question: "Qual é o maior planeta do sistema solar?",
-    answer: "Júpiter",
-    options: [
-      "Vênus",
-      "Marte",
-      "Júpiter",
-      "Saturno"
-    ]
-  },
-  {
-    numb: 7,
-    question: "Quem é o autor de 'Romeu e Julieta'?",
-    answer: "William Shakespeare",
-    options: [
-      "Charles Dickens",
-      "Jane Austen",
-      "William Shakespeare",
-      "Mark Twain"
-    ]
-  },
-];
